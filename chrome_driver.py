dab7d8491bf79b275644124b122f000e8845fa30
nisan <nzwnisan@gmail.com>
2025-03-31 18:48:12 +0800

    add

--- chrome_driver.py
+++ chrome_driver.py
@@ -70,7 +70,7 @@ def get_chrome_driver(proxy=None):
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.165 Safari/537.36")
     chrome_options.add_argument("window-size=1920,3000")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
